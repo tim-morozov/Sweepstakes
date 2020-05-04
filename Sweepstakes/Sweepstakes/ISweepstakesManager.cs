6e421733bc16887bca397a0b079a1df82f3c4b9d
tim-morozov <tim.morozov@gmail.com>
2020-05-04 15:58:34 -0500

    created SweepStakesStackManager and SweepstakesQueueManager classes and made them implement ISweepstakesManager

--- Sweepstakes/Sweepstakes/ISweepstakesManager.cs
+++ Sweepstakes/Sweepstakes/ISweepstakesManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    interface ISweepstakesManagercs
+    interface ISweepstakesManager
     {
         void InsertSweepstakes(Sweepstakes sweepstakes);
         Sweepstakes GetSweepstakes();
