86607d49d8466c51e4ab46df4e6138211aac2c24
tim-morozov <tim.morozov@gmail.com>
2020-05-04 15:04:37 -0500

    added Contestant class with basic member variables

---
+++ Sweepstakes/Sweepstakes/Contestant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        string firstName;
+        string lastName;
+        string email;
+        int registrationNum;
+    }
+}
