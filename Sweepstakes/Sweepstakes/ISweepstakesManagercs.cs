2fa7968998c09d827a3e0f99b700bb315b463ac9
tim-morozov <tim.morozov@gmail.com>
2020-05-04 15:47:19 -0500

    added Sweepstakes class, and a ISweepstakesManager interface with two methods

---
+++ Sweepstakes/Sweepstakes/ISweepstakesManagercs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    interface ISweepstakesManagercs
+    {
+        void InsertSweepstakes(Sweepstakes sweepstakes);
+        Sweepstakes GetSweepstakes();
+    }
+}
